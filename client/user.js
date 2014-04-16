5d1fd879a59749d742081844ae0d303717b857ad
zeekay <zk@monoid.io>
2014-04-16 07:03:04 -0500

    No need to wrap user model in a closure.

--- client/user.js
+++ client/user.js
@@ -1,51 +1,47 @@
-module.exports = (function User() {
-
-  function User (options) {
-    options = options || {};
-    this.rtc = options.rtc,
-    this.name = options.name || '';
-    this.el = options.el || document.createElement('video');
-    this.el.id = 'video_'+options.id;
-    this.streamOptions = { 
-      'video': true, 
-      'audio': false 
-    };
-
-    this.render();
+function User (options) {
+  options = options || {};
+  this.rtc = options.rtc,
+  this.name = options.name || '';
+  this.el = options.el || document.createElement('video');
+  this.el.id = 'video_'+options.id;
+  this.streamOptions = {
+    'video': true,
+    'audio': false
   };
 
-  User.prototype.render = function () {
-    document.body.appendChild(this.el);
-  };
+  this.render();
+};
 
-  User.prototype.connect = function (cb) {
-    var _this = this;
-    console.log('connect', this);
-    return this.rtc.createStream(this.streamOptions, (function (stream) {
-      this.attachStream(stream);
-      if (cb) { cb(stream); }
-    }).bind(this));
-  };
+User.prototype.render = function () {
+  document.body.appendChild(this.el);
+};
 
-  User.prototype.attachStream = function (stream) {
-    // get local stream for manipulation
-    this.stream = stream;
-    this.rtc.attachStream(this.stream, this.el.id);
-    this.el.play();
-  };
+User.prototype.connect = function (cb) {
+  var _this = this;
+  console.log('connect', this);
+  return this.rtc.createStream(this.streamOptions, (function (stream) {
+    this.attachStream(stream);
+    if (cb) { cb(stream); }
+  }).bind(this));
+};
 
-  User.prototype.sync = function () {
-    console.log('sync', this);  
-  };
+User.prototype.attachStream = function (stream) {
+  // get local stream for manipulation
+  this.stream = stream;
+  this.rtc.attachStream(this.stream, this.el.id);
+  this.el.play();
+};
 
-  User.prototype.say = function () {
-    console.log('connect', this);
-  };
+User.prototype.sync = function () {
+  console.log('sync', this);
+};
 
-  User.prototype.takeback = function () {
-    throw new Error('Sorry, no take-backs');
-  };
+User.prototype.say = function () {
+  console.log('connect', this);
+};
 
-  return User;
+User.prototype.takeback = function () {
+  throw new Error('Sorry, no take-backs');
+};
 
-})();
+module.exports = User
