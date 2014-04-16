63dac579ea69df972b4d5f0fa9778d111c2069bc
Russell Madsen <ruzz311@gmail.com>
2014-04-16 00:17:36 -0500

    added user and app model to client.js

--- client/index.js
+++ client/index.js
@@ -1,21 +1,35 @@
+var User = require('./user.js');
 var rtc = require('./webrtc.io');
+var app = window.app = {
+  port: '8001',
+  host: 'localhost',
+  user: null,
+  peers: []
+};
 
-rtc.connect('ws://192.168.7.28:8001');
+rtc.connect('ws://'+app.host+':'+app.port);
 
-rtc.createStream({"video": true, "audio":false}, function(stream){
-  // get local stream for manipulation
-  rtc.attachStream(stream, 'local');
-});
+window.initUser = function () {
+  var user = app.user = new User({
+    id: 'local',
+    name: 'Chika, Chicka, Slim-shady',
+    rtc: rtc
+  });
 
-var clients = 0;
+  user.connect(function () {
+    console.log('user connected, stream is auto-attached', arguments);
+    debugger;
+  });
+}
 
-rtc.on('add remote stream', function(stream){
-  var video = document.createElement('video');
+initUser();
 
-  video.id = clients++;
-
-  document.body.appendChild(video);
+rtc.on('add remote stream', function (stream) {
+  var peer = new User({
+    id: app.peers.length,
+    rtc: rtc
+  });
 
   // show the remote video
-  rtc.attachStream(stream, video.id);
+  peer.attachStream(stream);
 });
---
+++ client/user.js
@@ -0,0 +1,51 @@
+module.exports = (function User() {
+
+  function User (options) {
+    options = options || {};
+    this.rtc = options.rtc,
+    this.name = options.name || '';
+    this.el = options.el || document.createElement('video');
+    this.el.id = 'video_'+options.id;
+    this.streamOptions = { 
+      'video': true, 
+      'audio': false 
+    };
+
+    this.render();
+  };
+
+  User.prototype.render = function () {
+    document.body.appendChild(this.el);
+  };
+
+  User.prototype.connect = function (cb) {
+    var _this = this;
+    console.log('connect', this);
+    return this.rtc.createStream(this.streamOptions, (function (stream) {
+      this.attachStream(stream);
+      if (cb) { cb(stream); }
+    }).bind(this));
+  };
+
+  User.prototype.attachStream = function (stream) {
+    // get local stream for manipulation
+    this.stream = stream;
+    this.rtc.attachStream(this.stream, this.el.id);
+    this.el.play();
+  };
+
+  User.prototype.sync = function () {
+    console.log('sync', this);  
+  };
+
+  User.prototype.say = function () {
+    console.log('connect', this);
+  };
+
+  User.prototype.takeback = function () {
+    throw new Error('Sorry, no take-backs');
+  };
+
+  return User;
+
+})();
