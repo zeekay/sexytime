71d45021610171d3d3ea5cb93d5bf895d2ae9e50
Russell Madsen <ruzz311@gmail.com>
2014-04-16 22:06:22 -0500

    cleanup user init

--- client/index.js
+++ client/index.js
@@ -9,20 +9,16 @@ var app = window.app = {
 
 rtc.connect('ws://'+app.host+':'+app.port);
 
-window.initUser = function () {
-  var user = app.user = new User({
-    id: 'local',
-    name: 'Chika, Chicka, Slim-shady',
-    rtc: rtc
-  });
+var user = app.user = new User({
+  id: 'local',
+  name: 'Chika, Chicka, Slim-shady',
+  rtc: rtc
+});
 
-  user.connect(function () {
-    console.log('user connected, stream is auto-attached', arguments);
-    debugger;
-  });
-}
-
-initUser();
+user.connect(function () {
+  console.log('user connected, stream is auto-attached', arguments);
+  debugger;
+});
 
 rtc.on('add remote stream', function (stream) {
   var peer = new User({
